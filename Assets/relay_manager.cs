dd9e003832c9637b2b51ae29dc9cad652edd7c55
ediardi <114097608+ediardi@users.noreply.github.com>
2024-02-29 21:14:33 +0200

    finally working on webgl

--- Assets/relay_manager.cs
+++ Assets/relay_manager.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using TMPro;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Relay;
@@ -38,19 +39,23 @@ public class relay_manager : MonoBehaviour
         _buttons.SetActive(false);
 
         Allocation a = await RelayService.Instance.CreateAllocationAsync(MaxPlayers);
+        _transport.SetRelayServerData(new RelayServerData(a, "wss"));
+        _transport.UseWebSockets = true;
+        _transport.SetRelayServerData(new RelayServerData(a, "wss"));
         _joinCodeText.text = await RelayService.Instance.GetJoinCodeAsync(a.AllocationId);
 
-        _transport.SetHostRelayData(a.RelayServer.IpV4, (ushort)a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData);
-
+        //_transport.SetHostRelayData(a.RelayServer.IpV4, (ushort)a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData);
         NetworkManager.Singleton.StartHost();
     }
     public async void JoinGame()
     {
         _buttons.SetActive(false);
-
+        
         JoinAllocation a = await RelayService.Instance.JoinAllocationAsync(_joinInput.text);
 
         _transport.SetClientRelayData(a.RelayServer.IpV4, (ushort)a.RelayServer.Port, a.AllocationIdBytes,a.Key, a.ConnectionData, a.HostConnectionData);
+
+        _transport.SetRelayServerData(new RelayServerData(a, "wss"));
 
         NetworkManager.Singleton.StartClient();
     }
